6a6288b2aabd9f309acbcb4ffc761bd0bf944a36
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-03-08 14:58:46 +0500

    bug fixed

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ const recognition=new SpeechRecognition();
 recognition.onstart=function () {
     console.log('voice is actavated, you can to microphone');
 }
+recognition.onresult = function(event){
+    const current = event.resultIndex;
+    const transcript=event.results[current][0].transcript;
+    content.textContent=transcript;
+}
 btn.addEventListener('click',()=>{
     recognition.start();
 })
